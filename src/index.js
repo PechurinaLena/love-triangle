8f499989fe816710a3c46004a290928bb9562270
PechurinaLena <lena8way@gmail.com>
2019-03-18 15:21:15 +0300

    test

--- src/index.js
+++ src/index.js
@@ -7,20 +7,10 @@ module.exports = function getLoveTrianglesCount(preferences = []) {
 
 	let count = 0;
 
-<<<<<<< HEAD
 	for (let i = 0; i <= preferences.length; i++) {
 		let a = preferences[i] - 1;
 		let b = preferences[a] - 1;
 		let c = preferences[b] - 1;
-=======
-  preferences.forEach(function getLoveTriangle(count) {
-    for (let a = 1; a < preferences.length; a++) {
-      let b = preferences[a];
-      let c = preferences[b];
-       a == preferences[c];
-    }
-  })
->>>>>>> 3729d3ae531e091a1fad594d9d411caeca870547
 
 		if (c == i) {
 			count++;
